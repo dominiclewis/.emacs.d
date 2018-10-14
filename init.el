c340b55c97e2b14fb5728c6b82a4e5e60d16d03a
Dominic Lewis <domlewis@cisco.com>
2018-10-14 17:31:48 +0100

    Init and docu change

--- init.el
+++ init.el
@@ -1,4 +1,29 @@
-;;Line Numbers
+;;Pacakge configs
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+(package-initialize)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+;; Theme
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-one t))
+
+;; UI
+(menu-bar-mode   -1)
+
+;; Line Numbers
 (when (version<= "26.0.50" emacs-version )
   (global-display-line-numbers-mode))
 
@@ -8,3 +33,18 @@
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-switchb)
 
+
+;;Delete this if there's any issues (it will be generated again I think)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (doom-themes use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;;DELETE UNTILL HERE
