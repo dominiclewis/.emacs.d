cdd5cbfc2e7015b19d1ab17d62dc3c9294e0cf97
Dominic Lewis <domlewis@cisco.com>
2018-10-17 22:48:41 +0100

    Revert "tmp"
    This reverts commit e9f443502f1c6d308adfdfc1fbbbcedc2a550f05.

--- init.el
+++ init.el
@@ -15,9 +15,11 @@
 (require 'use-package)
 
 ;; Theme
-(use-package spacemacs-theme
-  :defer t
-  :init (load-theme 'spacemacs-dark t))
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-one t))
+
 ;; Company - Autocompletion
 (use-package company
   :ensure t
@@ -36,12 +38,17 @@
 	:config
 	(which-key-mode))
 
+;;Org Bullets - Pretty Bullets
+(use-package org-bullets
+        :ensure t
+        :config
+        (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 ;; UI
 (menu-bar-mode   -1)
 
 ;; Line Numbers
-;;(when (version<= "26.0.50" emacs-version )
-;;    (global-display-line-numbers-mode))
+(when (version<= "26.0.50" emacs-version )
+    (global-display-line-numbers-mode))
 
 ;;Column Mode
 (setq column-number-mode t)
@@ -52,14 +59,13 @@
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-switchb)
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (ample-theme which-key use-package try org-bullets doom-themes company))))
+ '(package-selected-packages (quote (doom-themes use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
