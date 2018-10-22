58d46a9f7642ce55fa7b6c733b8b247236185f2a
Dominic Lewis <domlewis@cisco.com>
2018-10-22 09:42:49 +0100

    Swiper/Ivy/Counsel and org Bullets removed

--- init.el
+++ init.el
@@ -62,11 +62,40 @@
     (add-hook 'prog-mode-hook 'flyspell-prog-mode)
     (add-hook 'text-mode-hook 'flyspell-mode)
     ))
-;;Org Bullets - Pretty Bullets
-;(use-package org-bullets
-;        :ensure t
-;        :config
-;        (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+;;Swiper / Ivy / Counsel
+   (use-package counsel
+:ensure t
+  :bind
+  (("M-y" . counsel-yank-pop)
+   :map ivy-minibuffer-map
+   ("M-y" . ivy-next-line)))
+
+  (use-package ivy
+  :ensure t
+  :diminish (ivy-mode)
+  :bind (("C-x b" . ivy-switch-buffer))
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "%d/%d ")
+  (setq ivy-display-style 'fancy))
+
+  (use-package swiper
+  :ensure t
+  :bind (("C-s" . swiper)
+	 ("C-r" . swiper)
+	 ("C-c C-r" . ivy-resume)
+	 ("M-x" . counsel-M-x)
+	 ("C-x C-f" . counsel-find-file))
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (setq ivy-display-style 'fancy)
+    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+    ))
+
 ;; UI
 (menu-bar-mode   -1)
 
